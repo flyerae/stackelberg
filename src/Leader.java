dc36dad7b1a0766242f82d415033d99f4eaf22e6
Sam Bell <samueljamesbell@gmail.com>
2013-03-22 19:17:06 +0000

    Deleted junk from Leader

--- src/Leader.java
+++ src/Leader.java
@@ -6,50 +6,22 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * A very simple leader implementation that only generates random prices
- * @author Xin
- */
-final class Leader
-	extends PlayerImpl
+final class Leader extends PlayerImpl
 {
-	/* The randomizer used to generate random price */
-	private final Random m_randomizer = new Random(System.currentTimeMillis());
-
-	private Leader()
-		throws RemoteException, NotBoundException
+	private Leader() throws RemoteException, NotBoundException
 	{
 		super(PlayerType.LEADER, "Simple Leader");
 	}
 
 	@Override
-	public void goodbye()
-		throws RemoteException
+	public void goodbye() throws RemoteException
 	{
 	}
 
-	/**
-	 * To inform this instance to proceed to a new simulation day
-	 * @param p_date The date of the new day
-	 * @throws RemoteException
-	 */
 	@Override
-	public void proceedNewDay(int p_date)
-		throws RemoteException
+	public void proceedNewDay(int p_date) throws RemoteException
 	{
-		m_platformStub.publishPrice(m_type, genPrice(1.8f, 0.05f));
-	}
-
-	/**
-	 * Generate a random price based Gaussian distribution. The mean is p_mean,
-	 * and the diversity is p_diversity
-	 * @param p_mean The mean of the Gaussian distribution
-	 * @param p_diversity The diversity of the Gaussian distribution
-	 * @return The generated price
-	 */
-	private float genPrice(final float p_mean, final float p_diversity)
-	{
-		return (float) (p_mean + m_randomizer.nextGaussian() * p_diversity);
+		m_platformStub.publishPrice(m_type, 1.0f);
 	}
 
 	public static void main(final String[] p_args)
