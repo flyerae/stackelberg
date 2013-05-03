9a472f41ab96f088e361dde233821c49c14b9994
Danyal Prout <Danyal@Danyals-MacBook-Air.local>
2013-05-03 15:44:35 +0100

    Crossover started

--- src/Leader.java
+++ src/Leader.java
@@ -26,56 +26,89 @@ final class Leader extends PlayerImpl
     super(PlayerType.LEADER, "Leader");
   }
 
-  private void naiveWindowSize() throws RemoteException {
+  private void naiveWindowSize() {
     double currentError, minimumError = Double.POSITIVE_INFINITY;
     int optimalSize = 0;
-    
+
     for (windowSize = 1; windowSize <= MAX_WINDOW_SIZE; ++windowSize) {
       currentError = 0;
       for (int day = windowSize + 1; day <= 60; ++day) {
         findReactionFunction(day);
-        Record currentDay = m_platformStub.query(this.m_type, day);
+        Record currentDay = query(day);
         double followerPrice = payoff.followerEstimate(currentDay.m_leaderPrice);
         currentError += Math.abs(followerPrice - currentDay.m_followerPrice); 
       }
       currentError /= 60 - windowSize;
-      
-      // Happy with this
+
       if (currentError < minimumError) {
         minimumError = currentError;
         optimalSize = windowSize;
         System.out.println("New Minimum: " + optimalSize);
       }
-      
+
       System.out.printf("Size: %2d Current: %.5f Best: %.5f\n", windowSize, currentError, minimumError);
     }
     windowSize = optimalSize;
   }
 
-  @Override
-  public void startSimulation(final int p_steps) throws RemoteException {
-    naiveWindowSize();
+  private Record[] cache = new Record[91];
+
+  private Record query(int day) {
+    if (cache[day] == null)  {
+      try {
+        cache[day] = m_platformStub.query(this.m_type, day);
+      } catch (RemoteException e) {
+        System.out.println("Remote exception");
+      }
+    }
+    return cache[day];
+  }
+
+  private void crossValidation(int noFolds) { 
+    int currentFold = 0, currentDay = 0, foldSize = 60 / noFolds;
+    Record[][] folds = new Record[noFolds][foldSize];
+
+    for (int day = 1; day <= 60; ++day)  {
+      folds[currentFold][currentDay++] = query(day);
+      if (currentDay == foldSize) {
+        ++currentFold;
+        currentDay = 0;
+      }
+    }
+
+    for (int testingFold = 0; testingFold < foldSize; ++testingFold) {
+      for (int fold = 0; ) {
+      }
+    }
+     
+    this.windowSize = 10;
   }
 
   @Override
-  public void endSimulation() throws RemoteException {
-    //TODO: Think about.
-  }
+    public void startSimulation(final int p_steps) {
+      //naiveWindowSize();
+      crossValidation(5);
+    }
 
   @Override
-  public void proceedNewDay(int p_date) throws RemoteException {
-    findReactionFunction(p_date);
-    m_platformStub.publishPrice(m_type, payoff.globalMaximum());
-  }
+    public void endSimulation() {
+      //TODO: Think about.
+    }
 
-  private void findReactionFunction(int endDate) throws RemoteException {
+  @Override
+    public void proceedNewDay(int p_date) throws RemoteException {
+      findReactionFunction(p_date);
+      m_platformStub.publishPrice(m_type, payoff.globalMaximum());
+    }
+
+  private void findReactionFunction(int endDate) {
     double sumXSquared = 0;
     double sumY = 0;
     double sumX = 0;
     double sumXsumY = 0;
 
     for (int date = endDate - windowSize; date < endDate; ++date) {
-      Record day = m_platformStub.query(this.m_type, date);
+      Record day = query(date);
       sumX += day.m_leaderPrice;
       sumY += day.m_followerPrice;
       sumXSquared += Math.pow(day.m_leaderPrice, 2);
