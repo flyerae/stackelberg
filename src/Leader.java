21771cc00b82dbd9063e2d231729341701bbc1b1
Danyal <me@dany.al>
2013-05-05 21:43:04 +0100

    Cross validation working. FindReactionFunction always works off cache, cross-validation rearranges cache. Refactored error calculation into own function. Fixed incorrect initial sizing of cache.

--- src/Leader.java
+++ src/Leader.java
@@ -21,10 +21,15 @@ final class Leader extends PlayerImpl
   private Payoff payoff = null;
 
   private int windowSize;
-  private final static int MAX_WINDOW_SIZE = 59;
+  private final static int MAX_WINDOW_SIZE = 49;
 
   private Leader() throws RemoteException, NotBoundException {
     super(PlayerType.LEADER, "Leader");
+  }
+
+  private double calculateError(Record actual) {
+    double followerPrice = this.payoff.followerEstimate(actual.m_leaderPrice);
+    return Math.abs(followerPrice - actual.m_followerPrice);
   }
 
   private void naiveWindowSize() throws RemoteException {
@@ -35,9 +40,7 @@ final class Leader extends PlayerImpl
       currentError = 0.0;
       for (int day = windowSize + 1; day <= 60; ++day) {
         findReactionFunction(day);
-        Record currentDay = cache[day];
-        double followerPrice = payoff.followerEstimate(currentDay.m_leaderPrice);
-        currentError += Math.abs(followerPrice - currentDay.m_followerPrice);
+        currentError += calculateError(cache[day]);
       }
       currentError /= 60 - windowSize;
 
@@ -54,86 +57,81 @@ final class Leader extends PlayerImpl
 
   private Record[] cache;
 
-  /*
-  private Record query(int day) {
-    if (cache[day] == null)  {
-      try {
-        cache[day] = m_platformStub.query(this.m_type, day);
-      } catch (RemoteException e) {
-        System.out.println("Remote exception");
-      }
+  // Rearranges the cache for cross-validation.
+  private void rearrangeCache(int testingFold) {
+    for (int date = 1; date <= foldSize(); ++date) {       // For each element in the fold.
+      int testing = foldSize() * testingFold + date;       // Index of the element to move.
+      int training = 60 - foldSize() + date;               // Index to move it to.
+      swapCache(testing, training);                        // Swap the two elements.
     }
-    return cache[day];
-  }
-  */
-
-  private void crossValidation(int noFolds) { 
-    int currentFold = 0, currentDay = 0, foldSize = 60 / noFolds;
-    Record[][] folds = new Record[noFolds][foldSize];
-
-    for (int day = 1; day <= 60; ++day)  {
-      folds[currentFold][currentDay++] = cache[day];
-      if (currentDay == foldSize) {
-        ++currentFold;
-        currentDay = 0;
-      }
-    }
-    
-    // test on each fold
-    for (int testingFold = 0; testingFold < noFolds; ++testingFold) {
-      System.out.printf("Fold %d..\n", testingFold);
-      
-      // training data made of noFolds - 1 folds
-      Record[] training = new Record[60 - foldSize];
-      
-      for (int i = 0, j = 0; i < noFolds - 1; ++i)
-        if (i != testingFold) System.arraycopy(folds[i], 0, training, (j++)*foldSize, foldSize);
-      
-      // TODO: train & test:
-      // pass training array to findReactionFunction()
-      // test on folds[testingFold] to find avg. error
-    }
-    
-    System.out.println("Ended");
-     
-    this.windowSize = 10;
   }
 
-  @Override
-    public void startSimulation(final int p_steps) throws RemoteException {
-      this.cache = new Record[61 + p_steps];
-      
-      for (int day = 1; day <= 60; ++day)
-        this.cache[day] = m_platformStub.query(this.m_type, day);
-      
-      //naiveWindowSize();
-      crossValidation(5);
+  // Reapplying the rearrangement reverses it.
+  private void resetCache(int testingFold) {
+    rearrangeCache(testingFold);
+  }
+
+  private void swapCache(int a, int b) {
+    Record temp = cache[a];
+    cache[a] = cache[b];
+    cache[b] = temp;
+  }
+
+  private final int NO_FOLDS = 5;
+  private int foldSize() { return 60 / NO_FOLDS; }
+
+  private void crossValidation() { 
+    double[] error = new double[MAX_WINDOW_SIZE + 1];
+    for (int testingFold = 0; testingFold < NO_FOLDS; ++testingFold) {
+      rearrangeCache(testingFold);
+      for (windowSize = 1; windowSize <= MAX_WINDOW_SIZE; ++windowSize) {
+        for (int testing = 50; testing <= 60; ++testing) {
+          findReactionFunction(testing);
+          error[windowSize] += calculateError(cache[testing]);
+        }
+      }
+      resetCache(testingFold);
     }
 
-  @Override
-    public void endSimulation() {
-      //TODO: Think about.
-    }
+    int minimum = 1;
+    for (int i = 2; i < error.length; ++i)
+      if (error[i] < error[minimum])
+        minimum = i;
+
+    this.windowSize = minimum;
+  }
 
   @Override
-    public void proceedNewDay(int p_date) throws RemoteException {
-      findReactionFunction(p_date);
-      m_platformStub.publishPrice(m_type, payoff.globalMaximum());
-      this.cache[p_date] = m_platformStub.query(this.m_type, p_date);
-    }
+  public void startSimulation(final int p_steps) throws RemoteException {
+    this.cache = new Record[60 + p_steps];
+      
+    for (int day = 1; day <= 60; ++day)
+      this.cache[day] = m_platformStub.query(this.m_type, day);
+      
+    //naiveWindowSize();
+    crossValidation();
+  }
+
+  @Override
+  public void endSimulation() {
+    //TODO: Think about.
+  }
+
+  @Override
+  public void proceedNewDay(int p_date) throws RemoteException {
+    findReactionFunction(p_date);
+    m_platformStub.publishPrice(m_type, payoff.globalMaximum());
+    this.cache[p_date] = m_platformStub.query(this.m_type, p_date);
+  }
 
   private void findReactionFunction(int endDate) {
-    findReactionFunction(endDate, this.cache);
-  }
-  
-  private void findReactionFunction(int endDate, Record[] data) {
     double sumXSquared = 0;
     double sumY        = 0;
     double sumX        = 0;
     double sumXsumY    = 0;
 
     for (int date = endDate - windowSize; date < endDate; ++date) {
-      Record day   = data[date];
+      Record day   = this.cache[date];
       sumX        += day.m_leaderPrice;
       sumY        += day.m_followerPrice;
       sumXSquared += Math.pow(day.m_leaderPrice, 2);
